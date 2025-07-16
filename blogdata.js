49e12069b68a84844232f1bfd5e27e390a31117a
mimi08-coder <phantienminh30122008bo@gmail.com>
2025-07-16 17:13:03 +0700

    update division

--- blogdata.js
+++ blogdata.js
@@ -4,7 +4,7 @@ const blogPosts = {
       id: "convex-hull",
       title: "Convex Hull Trick Explained",
       desc: "A deep dive into optimization techniques using convex hulls.",
-      link: "page.html"
+      link: "algo/page.html"
     },
     {
       id: "li-chao-tree",
